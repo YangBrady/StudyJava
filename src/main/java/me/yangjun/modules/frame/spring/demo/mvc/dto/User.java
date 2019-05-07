24eec8818a35253d8b19126c06716b41d9a6e800
mooejun <mooejun@163.com>
2019-05-07 20:06:25 +0800

    //== 新增bean验证

--- src/main/java/me/yangjun/modules/frame/spring/demo/mvc/dto/User.java
+++ src/main/java/me/yangjun/modules/frame/spring/demo/mvc/dto/User.java
@@ -1,5 +1,6 @@
 package me.yangjun.modules.frame.spring.demo.mvc.dto;
 
+import javax.validation.constraints.NotEmpty;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -10,6 +11,7 @@ public class User {
 
     private String id;
 
+    @NotEmpty
     private String username;
 
     public User() {
---
+++ src/main/java/me/yangjun/modules/frame/spring/demo/mvc/interceptor/MyInterceptor.java
@@ -0,0 +1,32 @@
+package me.yangjun.modules.frame.spring.demo.mvc.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 自定义拦截器
+ * 需要实现HandlerInterceptor并在mvc配置文件中配置
+ *
+ * @author mooejun
+ * @since 2019/05/07
+ */
+public class MyInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("-----preHandle-----");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("-----postHandle-----");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("-----afterCompletion-----");
+    }
+}
