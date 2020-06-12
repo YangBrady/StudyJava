a437efe56797fd20adc45c9da1f7a7cf522d8ca3
mooejun <mooejun@163.com>
2020-06-12 16:48:32 +0800

    //== 20200612

---
+++ src/main/java/me/yangjun/modules/leetcode/editor/cn/ListNode.java
@@ -0,0 +1,14 @@
+package me.yangjun.modules.leetcode.editor.cn;
+
+/**
+ * @author mooejun
+ * @since 2020/06/03 11:50
+ */
+public class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+	}
+}
