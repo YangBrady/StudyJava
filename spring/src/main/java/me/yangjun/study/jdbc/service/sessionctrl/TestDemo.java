e358ddcddd1e2e48a42339bc5c81a4e88cc20268
mooejun <2591577190@qq.com>
2020-07-06 16:58:09 +0800

    //== 事务控制测试

--- spring/src/main/java/me/yangjun/study/jdbc/service/sessionctrl/TestDemo.java
+++ spring/src/main/java/me/yangjun/study/jdbc/service/sessionctrl/TestDemo.java
@@ -1,57 +1,37 @@
 package me.yangjun.study.jdbc.service.sessionctrl;
 
-import me.yangjun.study.jdbc.bean.TestBean;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestDemo {
 
-	@Test
-	public void testDemo001() {
+	private static TestService aTestService;
+
+	static {
 		ApplicationContext ac = new ClassPathXmlApplicationContext("classpath*:jdbc/applicationContext.xml");
-		TestService aTestService = (TestService) ac.getBean("testService");
-		TestBean aTestBean = new TestBean("东东", "1");
-		aTestService.normalErrorSave(aTestBean);
+		aTestService = (TestService) ac.getBean("testService");
 	}
 
 	@Test
-	public void testDemo002() {
-		ApplicationContext ac = new ClassPathXmlApplicationContext("classpath*:jdbc/applicationContext2.xml");
-		TestService aTestService = (TestService) ac.getBean("testService");
-		TestBean aTestBean = new TestBean("东东", "1");
-		aTestService.sessionSave(aTestBean);
-	}
+	public void normalSave() {
+		aTestService.normalSave();
+		try {
+			aTestService.normalSaveError();
+		} catch (Exception e) {}
+
+		aTestService.sessionRequired();
+		try {
+			aTestService.sessionRequiredError();
+		} catch (Exception e) {}
 
 
-	@Test
-	public void testDemo003() {
-		ApplicationContext ac = new ClassPathXmlApplicationContext("classpath*:jdbc/applicationContext2.xml");
-		TestService aTestService = (TestService) ac.getBean("testService");
-		TestBean aTestBean = new TestBean("东东", "2");
-		aTestService.sessionErrorSave(aTestBean);
-	}
+		try {
+			aTestService.sessionNotSupportedError();
+		} catch (Exception e) {}
 
-	@Test
-	public void testDemo004() {
-		ApplicationContext ac = new ClassPathXmlApplicationContext("classpath*:jdbc/applicationContext2.xml");
-		TestService aTestService = (TestService) ac.getBean("testService");
-		TestBean aTestBean = new TestBean("西西", "1");
-		aTestService.sessionSave(aTestBean); // 会插入
-		TestBean aTestBean2 = new TestBean("西西", "2");
-		aTestService.sessionErrorSave(aTestBean2); // 不会插入
+		try {
+			aTestService.sessionReadOnly();
+		} catch (Exception e) {}
 	}
-
-	@Test
-	public void testDemo005() {
-		ApplicationContext ac = new ClassPathXmlApplicationContext("classpath*:jdbc/applicationContext2.xml");
-		TestService aTestService = (TestService) ac.getBean("testService");
-		TestBean aTestBean = new TestBean("西西1234", "1");
-		aTestService.sessionRequired(aTestBean); // 会插入
-		TestBean aTestBean2 = new TestBean("西西1234", "2");
-		aTestService.sessionErrorRequired(aTestBean2); // 不会插入
-	}
-	/**
-	 * 通过一个没有事务的方法调用一个有事务的方法（不会回滚）
-	 */
 }
--- spring/src/main/java/me/yangjun/study/jdbc/service/sessionctrl/TestService.java
+++ spring/src/main/java/me/yangjun/study/jdbc/service/sessionctrl/TestService.java
@@ -13,73 +13,69 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class TestService {
 
-    @Autowired
-    private TestDao testDao;
+	@Autowired
+	private TestDao testDao;
 
-    /**
-     * 模拟默认情况下报错
-     */
-    public void normalErrorSave(TestBean aTestBean) {
-        this.testDao.save(aTestBean); // 会入表
-        long a = 1 / 0;
-        this.testDao.save(aTestBean); // 不会入表
-    }
+	public void normalSave() {
+		TestBean aTestBean = new TestBean("normalSave", "1");
+		this.testDao.save(aTestBean); // 成功
+	}
 
-    /**
-     * 加入事务控制，默认事务控制
-     */
-    @Transactional()
-    public void sessionSave(TestBean aTestBean) {
-        this.testDao.save(aTestBean);
-    }
+	/**
+	 * 模拟默认情况下报错
+	 */
+	public void normalSaveError() {
+		TestBean aTestBean = new TestBean("normalSaveError", "1");
+		this.testDao.save(aTestBean); // 成功
+		long a = 1 / 0;
+		TestBean aTestBean2 = new TestBean("normalSaveError", "2");
+		this.testDao.save(aTestBean2); // 不会入表
+	}
 
-    /**
-     * 加入事务控制，默认事务控制，报错全部回滚
-     */
-    @Transactional()
-    public void sessionErrorSave(TestBean aTestBean) {
-        this.testDao.save(aTestBean); // 不会入表
-        long a = 1 / 0;
-        this.testDao.save(aTestBean); // 不会入表
-    }
+	/**
+	 * 有异常的话当前事务全部回滚
+	 * propagation ==> 事务传播级别
+	 * REQUIRED:加入当前已有事务
+	 * rollbackFor ==> 遇到指定异常回滚
+	 */
+	@Transactional(propagation = Propagation.REQUIRED, rollbackFor = Throwable.class)
+	public void sessionRequired() {
+		TestBean aTestBean = new TestBean("sessionRequired", "1");
+		this.testDao.save(aTestBean); // 成功
+	}
 
-    /**
-     * 有异常的话当前事务全部回滚
-     * propagation ==> 事务传播级别
-     *                 REQUIRED:加入当前已有事务
-     * rollbackFor ==> 遇到指定异常回滚
-     */
-    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Throwable.class)
-    public void sessionRequired(TestBean aTestBean) {
-        this.testDao.save(aTestBean);
-    }
+	@Transactional(propagation = Propagation.REQUIRED, rollbackFor = Throwable.class)
+	public void sessionRequiredError() {
+		TestBean aTestBean = new TestBean("sessionErrorRequired", "1");
+		this.testDao.save(aTestBean); // 不会入表
+		long a = 1 / 0;
+		TestBean aTestBean2 = new TestBean("sessionErrorRequired", "2");
+		this.testDao.save(aTestBean); // 不会入表
+	}
 
-    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Throwable.class)
-    public void sessionErrorRequired(TestBean aTestBean) {
-        this.testDao.save(aTestBean);
-        long a = 1 / 0;
-        this.testDao.save(aTestBean);
-    }
+	/**
+	 * 有异常的话当前事务全部回滚
+	 * propagation ==> 事务传播级别
+	 * NOT_SUPPORTED:以非事务方式执行
+	 */
+	@Transactional(propagation = Propagation.NOT_SUPPORTED, rollbackFor = Throwable.class)
+	public void sessionNotSupportedError() {
+		TestBean aTestBean = new TestBean("sessionNotSupported", "1");
+		this.testDao.save(aTestBean); // 成功
+		long a = 1 / 0;
+		TestBean aTestBean2 = new TestBean("sessionNotSupported", "2");
+		this.testDao.save(aTestBean2); // 不会入表
+	}
 
-    /**
-     * 有异常的话当前事务全部回滚
-     * propagation ==> 事务传播级别
-     *                 NOT_SUPPORTED:以非事务方式执行
-     */
-    @Transactional(propagation = Propagation.NOT_SUPPORTED, rollbackFor = Throwable.class)
-    public void sessionNotSupported(TestBean aTestBean) {
-        this.testDao.save(aTestBean);
-        long a = 1 / 0;
-        this.testDao.save(aTestBean);
-    }
-
-    /**
-     * 设置只读，默认false
-     */
-    @Transactional(readOnly = true)
-    public void sessionReadOnly(TestBean aTestBean) {
-        this.testDao.save(aTestBean);
-        long a = 1 / 0;
-        this.testDao.save(aTestBean);
-    }
+	/**
+	 * 设置只读，默认false
+	 */
+	@Transactional(readOnly = true)
+	public void sessionReadOnly() {
+		TestBean aTestBean = new TestBean("sessionReadOnly", "1");
+		this.testDao.save(aTestBean); // 不会入表
+		long a = 1 / 0;
+		TestBean aTestBean2 = new TestBean("sessionReadOnly", "2");
+		this.testDao.save(aTestBean2); // 不会入表
+	}
 }
