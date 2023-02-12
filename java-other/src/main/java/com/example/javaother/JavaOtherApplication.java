8d9bb0a40efa625fd8f718311537833a07af9562
YangBrady <mooejun@163.com>
2023-02-12 14:58:09 +0800

    other模块

---
+++ java-other/src/main/java/com/example/javaother/JavaOtherApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOtherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaOtherApplication.class, args);
+    }
+
+}
