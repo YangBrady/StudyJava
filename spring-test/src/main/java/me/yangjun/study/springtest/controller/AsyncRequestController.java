7ff7246bdf9fc727d2976be410bc598971587f8a
YangBrady <mooejun@163.com>
2023-05-04 23:47:36 +0800

    异步问题说明

--- spring-test/src/main/java/me/yangjun/study/springtest/controller/AsyncRequestController.java
+++ spring-test/src/main/java/me/yangjun/study/springtest/controller/AsyncRequestController.java
@@ -1,16 +1,20 @@
 package me.yangjun.study.springtest.controller;
 
-import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.concurrent.TimeUnit;
 
 /**
  * <p>描述：测试异步请求参数问题</p>
+ * <p>参考博客：
+ * <a href="https://www.cnblogs.com/thisiswhy/p/16539739.html">关于Request复用的那点破事儿。研究明白了，给你汇报一下。 </a>
+ * <a href="https://www.cnblogs.com/mysgk/p/16470336.html">springboot 中如何正确的在异步线程中使用request </a>
+ * </p>
  *
  * @author: Brady Yang
  * @date: 2023/3/12  17:44
@@ -54,5 +58,4 @@ public class AsyncRequestController {
         }).start();
         return "get success";
     }
-
 }
