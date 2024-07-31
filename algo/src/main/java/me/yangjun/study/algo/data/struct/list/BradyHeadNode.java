e18a0f143005b41a8909cb4cdee9e8f39928948c
YangBrady <mooejun@163.com>
2024-08-01 00:43:34 +0800

    链表反转

---
+++ algo/src/main/java/me/yangjun/study/algo/data/struct/list/BradyHeadNode.java
@@ -0,0 +1,32 @@
+package me.yangjun.study.algo.data.struct.list;
+
+import lombok.Data;
+
+@Data
+public class BradyHeadNode<T> {
+    private T value;
+    private BradyHeadNode<T> next;
+
+    public BradyHeadNode(T value) {
+        this.value = value;
+    }
+
+    public BradyHeadNode<T> reverse() {
+        BradyHeadNode<T> indexNode = this.next;
+        BradyHeadNode<T> restNode;
+        this.next = null;
+
+        BradyHeadNode<T> result = new BradyHeadNode<>(null);
+
+        while (indexNode != null) {
+            // 剩余部分
+            restNode = indexNode.next;
+            // 反转
+            indexNode.next = result;
+            // 指向新的链表头
+            result = indexNode;
+            indexNode = restNode;
+        }
+        return result.next;
+    }
+}
---
+++ algo/src/main/java/me/yangjun/study/algo/data/struct/list/LinkedListDemo.java
@@ -0,0 +1,18 @@
+package me.yangjun.study.algo.data.struct.list;
+
+public class LinkedListDemo {
+
+    public static void main(String[] args) {
+        BradyHeadNode<Integer> headNode = new BradyHeadNode<>(0);
+        BradyHeadNode<Integer> node1 = new BradyHeadNode<>(1);
+        headNode.setNext(node1);
+        BradyHeadNode<Integer> node2 = new BradyHeadNode<>(2);
+        node1.setNext(node2);
+        BradyHeadNode<Integer> node3 = new BradyHeadNode<>(3);
+        node2.setNext(node3);
+        BradyHeadNode<Integer> node4 = new BradyHeadNode<>(4);
+        node3.setNext(node4);
+
+        BradyHeadNode<Integer> reversedNode = headNode.reverse();
+    }
+}
