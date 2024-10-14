b0c8eb6efd65667c72ba4641a0c1abab9d0210cb
YangBrady <mooejun@163.com>
2024-10-14 23:22:40 +0800

    disruptor

--- java-other/src/main/java/com/example/javaother/disruptor/event/BradyEvent.java
+++ java-other/src/main/java/com/example/javaother/disruptor/event/BradyEvent.java
@@ -1,10 +1,8 @@
 package com.example.javaother.disruptor.event;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class BradyEvent {
     private String eventMsg;
 }
--- java-other/src/main/java/com/example/javaother/disruptor/test/DisruptorDemo.java
+++ java-other/src/main/java/com/example/javaother/disruptor/test/DisruptorDemo.java
@@ -1,9 +1,9 @@
 package com.example.javaother.disruptor.test;
 
 import com.example.javaother.disruptor.event.BradyEvent;
-import com.example.javaother.disruptor.event.BradyEventFactory;
-import com.example.javaother.disruptor.handler.BradyEventHandler;
 import com.lmax.disruptor.BlockingWaitStrategy;
+import com.lmax.disruptor.EventFactory;
+import com.lmax.disruptor.EventHandler;
 import com.lmax.disruptor.RingBuffer;
 import com.lmax.disruptor.dsl.Disruptor;
 import com.lmax.disruptor.dsl.ProducerType;
@@ -29,15 +29,17 @@ public class DisruptorDemo {
     }
 
     private static Disruptor<BradyEvent> getBradyEventDisruptor() {
-        BradyEventFactory eventFactory = new BradyEventFactory();
+        EventFactory<BradyEvent> eventFactory = BradyEvent::new;
         int ringBufferSize = 16;
         ThreadFactory threadFactory = r -> new Thread(r, "simpleThread");
+        ProducerType producerType = ProducerType.SINGLE;
         BlockingWaitStrategy waitStrategy = new BlockingWaitStrategy();
 
         Disruptor<BradyEvent> disruptor =
-            new Disruptor<>(eventFactory, ringBufferSize, threadFactory, ProducerType.SINGLE, waitStrategy);
+            new Disruptor<>(eventFactory, ringBufferSize, threadFactory, producerType, waitStrategy);
 
-        BradyEventHandler eventHandler = new BradyEventHandler();
+        EventHandler<BradyEvent> eventHandler =
+            (event, sequence, endOfBatch) -> System.out.println("on event: event" + event);
         disruptor.handleEventsWith(eventHandler);
         disruptor.start();
         return disruptor;
