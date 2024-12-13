ab808910fed08e6833ab8ee47d2d1f4a4c85aae4
brady mac <mooejun@163.com>
2024-12-13 13:29:25 +0800

    submit会把异常记录在返回值Future中

--- java-se/src/test/java/me/yangjun/study/juc/executor/ExecutorTest.java
+++ java-se/src/test/java/me/yangjun/study/juc/executor/ExecutorTest.java
@@ -1,6 +1,7 @@
 package me.yangjun.study.juc.executor;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.LinkedBlockingQueue;
@@ -59,5 +60,29 @@ public class ExecutorTest {
         for (int i = 0; i < 20; i++) {
             threadPoolExecutor.submit(() -> log.info("ok2"));
         }
+    }
+
+    @Test
+    @DisplayName("获取提交任务的堆栈信息")
+    void test003() throws InterruptedException {
+        ThreadPoolExecutor threadPoolExecutor =
+                new ThreadPoolExecutor(2, 5, 1, TimeUnit.DAYS, new LinkedBlockingQueue<>(10),
+                        new ThreadPoolExecutor.CallerRunsPolicy());
+        threadPoolExecutor.prestartCoreThread();
+        // 异常会被吞掉
+        // threadPoolExecutor.submit(() -> log.info(String.valueOf(10/2)));
+        // threadPoolExecutor.submit(() -> log.info(String.valueOf(0/1)));
+        // threadPoolExecutor.submit(() -> log.info(String.valueOf(10/0)));
+        // threadPoolExecutor.submit(() -> log.info(String.valueOf(5/2)));
+        // threadPoolExecutor.submit(() -> log.info(String.valueOf(1/2)));
+
+        // 异常会打印出来
+        threadPoolExecutor.execute(() -> log.info(String.valueOf(10/2)));
+        threadPoolExecutor.execute(() -> log.info(String.valueOf(0/1)));
+        threadPoolExecutor.execute(() -> log.info(String.valueOf(10/0)));
+        threadPoolExecutor.execute(() -> log.info(String.valueOf(5/2)));
+        threadPoolExecutor.execute(() -> log.info(String.valueOf(1/2)));
+
+        TimeUnit.SECONDS.sleep(3);
     }
 }
