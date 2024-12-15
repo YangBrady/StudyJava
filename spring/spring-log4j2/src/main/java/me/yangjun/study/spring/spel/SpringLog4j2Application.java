337c9f9fb1736f637c57fded7ad0d6b7569bc064
YangBrady <mooejun@163.com>
2024-12-15 15:10:12 +0800

    MDC

--- spring/spring-log4j2/src/main/java/me/yangjun/study/spring/spel/SpringLog4j2Application.java
+++ spring/spring-log4j2/src/main/java/me/yangjun/study/spring/spel/SpringLog4j2Application.java
@@ -1,15 +1,17 @@
-package me.yangjun.study.springlog4j2;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+package me.yangjun.study.spring.spel;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @Slf4j
 public class SpringLog4j2Application {
 
     public static void main(String[] args) {
+        MDC.put("userName", "哇哈哈");
+
         log.info("开始启动");
         SpringApplication.run(SpringLog4j2Application.class, args);
         log.info("结束启动");
