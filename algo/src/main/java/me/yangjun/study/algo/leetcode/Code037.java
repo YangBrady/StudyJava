b281b2b00fd4abc1a2ffa4e4dca7a551e5a1f9ef
Elead <yangjun@e-lead.cn>
2025-07-08 11:38:01 +0800

    第37题

---
+++ algo/src/main/java/me/yangjun/study/algo/leetcode/Code037.java
@@ -0,0 +1,52 @@
+package me.yangjun.study.algo.leetcode;
+
+public class Code037 {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 6};
+        int target = 2;
+        System.out.println(new Code037().searchInsert1(nums, target));
+        System.out.println(new Code037().searchInsert2(nums, target));
+    }
+
+    public int searchInsert1(int[] nums, int target) {
+        if (nums[0] > target) {
+            return 0;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= target) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+
+    public int searchInsert2(int[] nums, int target) {
+        if (nums[0] > target) {
+            return 0;
+        }
+        if (nums[nums.length - 1] < target) {
+            return nums.length;
+        }
+
+        int left = 0;
+        int right = nums.length - 1;
+        int mid = 0;
+
+        while (left <= right) {
+            mid = (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+            if (right - left <= 1) {
+                return right;
+            }
+        }
+
+        return mid;
+    }
+}
