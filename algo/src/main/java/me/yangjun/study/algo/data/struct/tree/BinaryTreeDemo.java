4f63b03fd13acf527e89b74aabfdc13bc7f3fe1d
Elead <yangjun@e-lead.cn>
2025-07-08 14:37:56 +0800

    二叉树遍历

--- algo/src/main/java/me/yangjun/study/algo/data/struct/tree/BinaryTreeDemo.java
+++ algo/src/main/java/me/yangjun/study/algo/data/struct/tree/BinaryTreeDemo.java
@@ -1,9 +1,9 @@
 package me.yangjun.study.algo.data.struct.tree;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.LinkedList;
 import java.util.Queue;
-
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * 二叉树 Demo
@@ -14,14 +14,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class BinaryTreeDemo {
     public static void main(String[] args) {
-        BinaryTreeDemo.breadthFirstTraversal001(TreeNode.initDefaultTree());
-        BinaryTreeDemo.depthFirstTraversal(TreeNode.initDefaultTree());
+//        BinaryTreeDemo.breadthFirstTraversal001(TreeNode.initDefaultTree());
+        BinaryTreeDemo.breadthFirstTraversal002(TreeNode.initDefaultTree());
+//        BinaryTreeDemo.depthFirstTraversal(TreeNode.initDefaultTree());
     }
 
     /**
-     * BFS 广度优先遍历，基于队列
-     *
-     * @param root 根节点
+     * BFS 第一种方式，最简单，但是没有记录深度
      */
     public static void breadthFirstTraversal001(TreeNode root) {
         // 定义一个队列，先进先出
@@ -41,8 +40,33 @@ public class BinaryTreeDemo {
     }
 
     /**
+     * BFS 第二种方式，最常用
+     */
+    public static void breadthFirstTraversal002(TreeNode root) {
+        // 定义一个队列，先进先出
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        int depth = 1;
+        while (!queue.isEmpty()) {
+            int length = queue.size();
+            for (int i = 0; i < length; i++) {
+                TreeNode node = queue.poll();
+                log.info("depth={}, node={}", depth, node);
+                if (node.getLeft() != null) {
+                    queue.offer(node.getLeft());
+                }
+                if (node.getRight() != null) {
+                    queue.offer(node.getRight());
+                }
+            }
+            depth++;
+        }
+    }
+
+    /**
      * DFS 深度优先遍历，基于递归
-     * 
+     *
      * <pre>
      *     前序遍历：根->左->右
      *     中序遍历：左->根->右
