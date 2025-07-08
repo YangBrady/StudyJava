32fce983194594c666f8dcc0e06f2bea8555e7a6
Elead <yangjun@e-lead.cn>
2025-07-08 15:15:40 +0800

    数组删除元素 - 快慢指针问题

---
+++ algo/src/main/java/me/yangjun/study/algo/leetcode/Code027.java
@@ -0,0 +1,27 @@
+package me.yangjun.study.algo.leetcode;
+
+public class Code027 {
+
+    public static void main(String[] args) {
+//        int[] nums = {3, 2, 2, 3};
+//        int target = 3;
+        int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
+        int target = 2;
+        System.out.println(new Code027().removeElement(nums, target));
+    }
+
+    public int removeElement(int[] nums, int val) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int slow = 0;
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (val == nums[fast]) {
+                continue;
+            }
+            nums[slow] = nums[fast];
+            slow++;
+        }
+        return slow;
+    }
+}
