b233300e33a7d090ea63194a01dce4759bca641d
Elead <yangjun@e-lead.cn>
2025-07-09 17:32:17 +0800

    迭代法 + 递归法

--- algo/src/main/java/me/yangjun/study/algo/leetcode/Code206.java
+++ algo/src/main/java/me/yangjun/study/algo/leetcode/Code206.java
@@ -9,9 +9,13 @@ public class Code206 {
         head.next.next.next = new ListNode(4);
         head.next.next.next.next = new ListNode(5);
 
-        System.out.println(new Code206().reverseList(head));
+//        System.out.println(new Code206().reverseList(head));
+        System.out.println(new Code206().reverseList2(head));
     }
 
+    /**
+     * 双指针法
+     */
     public ListNode reverseList(ListNode head) {
         // 当前节点
         ListNode cur = head;
@@ -26,6 +30,22 @@ public class Code206 {
             cur = temp;
         }
         return pre;
+    }
+
+    /**
+     * 递归法
+     */
+    public ListNode reverseList2(ListNode head) {
+        return reverse(null, head);
+    }
+
+    public ListNode reverse(ListNode first, ListNode end) {
+        if (end == null) {
+            return first;
+        }
+        ListNode temp = end.next;
+        end.next = first;
+        return reverse(end, temp);
     }
 
     static class ListNode {
