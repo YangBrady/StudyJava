cd48e90ed3d788bd6765033e1f596c1f2f82ff5f
Elead <yangjun@e-lead.cn>
2025-07-09 16:23:03 +0800

    两两交换

---
+++ algo/src/main/java/me/yangjun/study/algo/leetcode/Code024.java
@@ -0,0 +1,47 @@
+package me.yangjun.study.algo.leetcode;
+
+public class Code024 {
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println(new Code024().swapPairs(head));
+    }
+
+    public ListNode swapPairs(ListNode head) {
+        ListNode virNode = new ListNode(0, head);
+        ListNode pre = virNode;
+        while (pre.next != null && pre.next.next != null) {
+            ListNode p = pre.next;
+            ListNode q = p.next;
+
+            pre.next = q;
+            p.next = q.next;
+            q.next = p;
+
+            pre = pre.next.next;
+        }
+        return virNode.next;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
