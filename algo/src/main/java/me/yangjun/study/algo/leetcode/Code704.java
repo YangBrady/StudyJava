103b968ec128f5417a2ab454b01fd62c25f84c52
Elead <yangjun@e-lead.cn>
2025-07-09 14:14:09 +0800

    二分法

---
+++ algo/src/main/java/me/yangjun/study/algo/leetcode/Code704.java
@@ -0,0 +1,31 @@
+package me.yangjun.study.algo.leetcode;
+
+public class Code704 {
+
+    public static void main(String[] args) {
+        int[] nums = {-1,0,3,5,9,12};
+        int target = 2;
+        System.out.println(new Code704().search(nums, target));
+    }
+
+    /**
+     * 二分法
+     */
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int result = -1;
+        int mid = -1;
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+        }
+        return result;
+    }
+}
