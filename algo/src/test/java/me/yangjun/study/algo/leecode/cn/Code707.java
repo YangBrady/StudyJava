219df247aa69e0b1eeaae3854f861573a8bb1217
brady <2591577190@qq.com>
2025-07-19 17:40:59 +0800

    链表设计 707

--- algo/src/test/java/me/yangjun/study/algo/leecode/cn/Code707.java
+++ algo/src/test/java/me/yangjun/study/algo/leecode/cn/Code707.java
@@ -2,87 +2,108 @@ package me.yangjun.study.algo.leecode.cn;
 
 import me.yangjun.study.algo.leecode.cn.entity.ListNode;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
 
 /**
  * <a href="https://leetcode.cn/problems/design-linked-list/description/">LeeCode 707 </a>
  */
 public class Code707 {
-//    @Test
-//    public void test1() {
-//        MyLinkedList myLinkedList = new MyLinkedList();
-//        myLinkedList.addAtHead(1);
-//        System.out.println(myLinkedList);
-//        myLinkedList.addAtTail(3);
-//        System.out.println(myLinkedList);
-//    }
-//
-//    private static class MyLinkedList {
-//        ListNode head;
-//        int size;
-//
-//        public MyLinkedList() {
-//            this.size = 0;
-//        }
-//
-//        public int get(int index) {
-//            int i = 0;
-//            MyLinkedList tmp = this;
-//            while (i <= index) {
-//                if (this.next == null) {
-//                    return -1;
-//                }
-//                tmp = this.next;
-//                i++;
-//            }
-//            return tmp.val;
-//        }
-//
-//        public void addAtHead(int val) {
-//            addAtIndex(0, val);
-//        }
-//
-//        public void addAtTail(int val) {
-//            addAtIndex(size, val);
-//        }
-//
-//        public void addAtIndex(int index, int val) {
-//            if (index > size || index < 0) {
-//                return;
-//            }
-//
-//            size++;
-//
-//            ListNode dummy = new ListNode(0, head);
-//            ListNode addNode = new ListNode(val);
-//            ListNode temp = dummy;
-//            int i = 0;
-//            while (pre != null && i <= index) {
-//                ListNode nextNode = pre.next;
-//                ListNode rest = nextNode.next;
-//
-//                i++;
-//                pre = pre.next;
-//            }
-//        }
-//
-//        public void deleteAtIndex(int index) {
-//            MyLinkedList pre = this;
-//            MyLinkedList q = this;
-//            int i = 0;
-//            while (i <= index && q != null) {
-//                if (i == index) {
-//                    pre.next = q.next;
-//                } else {
-//                    pre = q;
-//                    q = q.next;
-//                    i++;
-//                }
-//            }
-//        }
-//    }
+    @Test
+    public void test1() {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.addAtHead(7);
+        myLinkedList.addAtHead(2);
+        myLinkedList.addAtHead(1);
+        myLinkedList.addAtIndex(3, 0);
+        myLinkedList.deleteAtIndex(2);
+        myLinkedList.addAtHead(6);
+        myLinkedList.addAtTail(4);
+        myLinkedList.addAtHead(4);
+        myLinkedList.addAtIndex(5, 0);
+        myLinkedList.addAtHead(6);
+        System.out.println(myLinkedList.head);
+    }
+
+    private static class MyLinkedList {
+        ListNode head;
+        int size;
+
+        public MyLinkedList() {
+            this.size = 0;
+            this.head = new ListNode(0);
+        }
+
+        public int get(int index) {
+            if (index < 0 || index > size) {
+                return -1;
+            }
+
+            int i = 0;
+            ListNode tmp = this.head;
+            while (i <= index) {
+                if (tmp.next == null) {
+                    return -1;
+                }
+                tmp = tmp.next;
+                i++;
+            }
+            return tmp.val;
+        }
+
+        public void addAtHead(int val) {
+            addAtIndex(0, val);
+        }
+
+        public void addAtTail(int val) {
+            addAtIndex(size, val);
+        }
+
+        public void addAtIndex(int index, int val) {
+            if (index > size || index < 0) {
+                return;
+            }
+
+            ListNode addNode = new ListNode(val);
+
+            int i = 0;
+            ListNode pre = this.head;
+            while (i <= index) {
+                if (pre == null) {
+                    return;
+                }
+                if (i == index) {
+                    ListNode tmp = pre.next;
+                    pre.next = addNode;
+                    addNode.next = tmp;
+                }
+                pre = pre.next;
+                i++;
+            }
+
+            size++;
+        }
+
+        public void deleteAtIndex(int index) {
+            if (index > size || index < 0) {
+                return;
+            }
+
+            ListNode pre = this.head;
+            int i = 0;
+            while (i <= index) {
+                if (pre == null) {
+                    return;
+                }
+                if (i == index) {
+                    ListNode cutNode = pre.next;
+                    if (cutNode == null) {
+                        return;
+                    }
+                    pre.next = cutNode.next;
+                }
+                pre = pre.next;
+                i++;
+            }
+            size--;
+        }
+    }
 }
