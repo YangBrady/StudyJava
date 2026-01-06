cdf41160e1e03567e7492f5e5e5ed7d6d0b3f861
YangBrady <mooejun@163.com>
2026-01-06 10:07:49 +0800

    代码优化

--- algo/src/test/java/me/yangjun/study/algo/leecode/cn/Code198.java
+++ algo/src/test/java/me/yangjun/study/algo/leecode/cn/Code198.java
@@ -31,12 +31,15 @@ public class Code198 {
             if (arr.length == 1) {
                 return arr[0];
             }
-            // dp[i] ?
+            // dp[i] ? i==arr.length-1
 
             // dp[0] = arr[0]
             // dp[1] = arr[0] || arr[1]
             // dp[2] = arr[1] || arr[0] + arr[2]
-            // dp[i] = Math.max(dp[i-1], dp[i-2]+arr[i])
+            // dp[3] = arr[1] + arr[3] || arr[0] + arr[2]
+
+            // dp[i] = 0到i-1 || 0到i-2 + 最后一个
+            // dp[i] = Math.max(dp[i-1], dp[i-2] + arr[i])
 
             // init dp[i]
             int dp0 = arr[0];
